bb052b7cfbc30b16086f1ad80f8eb7df555cdf80
Vlad Ivanovic <vladiim@me.com>
2013-05-23 08:21:15 +1000

    Photos complete

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require 'sinatra'
 require 'cloudinary'
 
 Cloudinary.config do |config|
-  config.cloud_name    = ENV['87_COUDINARY_CLOUD_NAME']
-  config.api_key       = ENV['87_CLOUDINARY_API_KEY']
-  config.api_secret    = ENV['87_CLOUDINARY_API_SECRET']
+  config.cloud_name    = ENV['COUDINARY_CLOUD_NAME_87NORTH']
+  config.api_key       = ENV['CLOUDINARY_API_KEY_87NORTH']
+  config.api_secret    = ENV['CLOUDINARY_API_SECRET_87NORTH']
   config.cdn_subdomain = true
 end
 
