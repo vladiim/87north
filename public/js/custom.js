6b231d9435ca7538e0e7f71463f4d1465f34001f
Vlad Ivanovic <vladiim@me.com>
2013-05-31 20:21:29 +1000

    Booking form validating

--- public/js/custom.js
+++ public/js/custom.js
@@ -16,3 +16,40 @@ jQuery(document).ready(function($) {
 		});
 
 });
+
+/* datepicker */
+
+$(function() {
+	$( "#datepicker" ).datepicker({ dateFormat: "dd/mm/yy" });
+});
+
+/* Angular js */
+
+function Ctrl($scope) {
+    MINIMUM_NIGHTS    = 3;
+    WINTER_NIGHT_COST = 220;
+    WINTER_WEEK_COST  = 1120;
+    SPRING_NIGHT_COST = 245;
+    SPRING_WEEK_COST  = 1330;
+    SUMMER_WEEK_COST  = 1890;
+
+	$scope.night_count = MINIMUM_NIGHTS;
+	$scope.user = {};
+	$scope.user = { startdate: new Date() };
+    $scope.user.cost = $scope.night_count * 220;
+
+	$scope.addNight = function() {
+		$scope.night_count  = $scope.night_count + 1;
+		$scope.changeCost();
+	}
+
+	$scope.minusNight = function() {
+		if ($scope.night_count > MINIMUM_NIGHTS) {
+			$scope.night_count = $scope.night_count - 1;
+		}
+	}
+
+	$scope.changeCost = function() {
+		$scope.user.cost = $scope.night_count * 220;
+	}
+}
